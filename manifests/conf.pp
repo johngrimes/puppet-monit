ced708177e6bb1ced72bf85f3d81000f370f3c93
John Grimes <john@smallspark.com.au>
2014-07-23 04:42:09 +0000

    Add ability to use template or file for Monit config

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -1,11 +1,26 @@
-define monit::conf($content) {
-  file { "/etc/monit/conf.d/${title}":
-    ensure  => file,
-    content => $content,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '640',
-    require => Package['monit'],
-    notify  => Service['monit']
+define monit::conf(
+  $content = nil,
+  $source = nil
+) {
+  if ($content) {
+    file { "/etc/monit/conf.d/${title}":
+      ensure  => file,
+      content => $content,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '640',
+      require => Package['monit'],
+      notify  => Service['monit']
+    }
+  } elsif ($source) {
+    file { "/etc/monit/conf.d/${title}":
+      ensure  => file,
+      source  => $source,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '640',
+      require => Package['monit'],
+      notify  => Service['monit']
+    }
   }
 }
